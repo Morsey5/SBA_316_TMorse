3c3cc0c0f5215fa3d89a5b4f4c5961e6e6a5ed57
Morsey5 <temeikamorse@gmail.com>
2023-10-29 13:40:09 -0400

    the button works

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 //Displays photos and allows them to be rated
 
+const photoContainer = document.getElementById('photoContainer');
 const photo = document.getElementById('photo');
 const thumbsUpButton = document.getElementById('thumbsUpButton');
 const thumbsDownButton = document.getElementById('thumbsDownButton');
@@ -8,31 +9,19 @@ let thumbsUpCount = 0;
 let thumbsDownCount = 0;
 
 //The photos
+//it was not working because I needed to seperate them with a coma
 
+
+//This needs to completely match the img link in HTML to work
 const photoUrls = [
-    'W_outdoors.jpeg'
-    'IMG_1229.jpeg'
-    'IMG_1332.JPG'
-    'IMG_1351.jpeg'
-    'IMG_1499.jpeg'
-    'IMG_1648.jpeg'
-    'IMG_1666.jpeg'
-    'MB_happy_pup.jpeg'
-    'IMG_1672.jpeg'
-    'IMG_1678.jpeg'
-    'IMG_1836.jpeg'
-    'IMG_2008.jpeg'
-    'IMG_2184.jpeg'
-    'IMG_2199.jpeg'
-    'IMG_2200.jpeg'
-    'IMG_2379.jpeg'
-    'IMG_2560.jpeg'
-    'IMG_2700.jpeg'
-    'IMG_2706.jpeg'
-    'IMG_2725.jpeg'
-    'IMG_2731.jpeg'
-    'BW_waiting.jpeg'
-    'Images/B_puppy_dog_eyes.jpeg'
+    'Images/B_puppy_dog_eyes.jpeg',
+    'Images/BW_waiting.jpeg',
+    'Images/IMG_1229.jpeg',
+    'Images/IMG_1332.JPG',
+    'Images/IMG_1351.jpeg',
+    'Images/IMG_1499.jpeg',
+    'Images/IMG_1648.jpeg',
+    'Images/IMG_1666.jpeg'
 ];
 
 let currentPhotoIndex = 0;
@@ -40,7 +29,7 @@ let currentPhotoIndex = 0;
 //Switches the photos
 
 function showNextPhoto() {
-    photo.style.display = 'block';
+   // photo.style.display = 'block';
     photo.src = photoUrls[currentPhotoIndex];
     currentPhotoIndex = (currentPhotoIndex + 1) % photoUrls.length;
 }
